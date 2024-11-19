a19495f3813136b442cf7707ab87b6b39928b846
FeToR1 <fedornigretuk@gmail.com>
2024-11-19 19:17:16 +0300

    added: __call__

--- experiments/sparse_and_edge_finder/nonlinearity_metrics.py
+++ experiments/sparse_and_edge_finder/nonlinearity_metrics.py
@@ -1,9 +1,10 @@
+from abc import abstractmethod, ABC
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class NonlinearityMetric:
+class NonlinearityMetric(ABC):
     def __init__(self, loss_fn):
         self.loss_fn = loss_fn
 
@@ -13,8 +14,21 @@ class NonlinearityMetric:
         #     raise ValueError("Expected a SparseRecursiveLinear model.")
         return model.sparse_linear
 
+    @abstractmethod
     def calculate(self, model, X_arr, y_arr):
-        raise NotImplementedError()
+        pass
+
+    def __call__(self, model, X_arr, y_arr, top_k: int = 1):
+        model.eval()
+        last_layer = self._get_last_sparse_layer(model)
+        values = self.calculate(model, X_arr, y_arr)
+
+        # Индексы, отсортированные по убыванию значений метрики
+        sorted_indices = torch.argsort(values, descending=True)
+
+        # Возвращаем топ k индексов из last_layer.weight_indices
+        top_k_indices = last_layer.weight_indices[sorted_indices[:top_k]]
+        return top_k_indices
 
 
 # Метрика 1: Средний градиент для каждого ребра
@@ -33,24 +47,6 @@ class GradientMeanEdgeMetric(NonlinearityMetric):
         edge_gradients = last_layer.weight_values.grad.abs()
         model.zero_grad()
         return edge_gradients
-
-
-# # Метрика 2: Стандартное отклонение активаций для каждого ребра
-# class ActivationStdEdgeMetric(NonlinearityMetric):
-#     def calculate(self, model, X_arr, y_arr):
-#         model.eval()
-#
-#         # Пропуск через модель
-#         activations = model(X_arr)
-#
-#         last_layer = self._get_last_sparse_layer(model)
-#
-#         # Для каждого ребра: находим соответствующие активации
-#         indices = last_layer.weight_indices
-#         print(indices, indices.shape, indices[1])
-#         edge_activations = activations[indices[1]] * last_layer.weight_values
-#         activation_std_edges = edge_activations.std(dim=0)
-#         return activation_std_edges
 
 
 # Метрика 3: Чувствительность к возмущению для каждого ребра
@@ -83,31 +79,3 @@ class PerturbationSensitivityEdgeMetric(NonlinearityMetric):
                 last_layer.weight_values[idx] = original_value
 
         return sensitivities
-
-
-# # Метрика 4: Косинусное расстояние между градиентами для каждого ребра
-# class CosineGradientSimilarityEdgeMetric(NonlinearityMetric):
-#     def calculate(self, model, X_arr, y_arr):
-#         model.eval()
-#         model.zero_grad()
-#
-#         y_pred = model(X_arr).squeeze()
-#         loss = self.loss_fn(y_pred, y_arr)
-#         loss.backward()
-#
-#         last_layer = self._get_last_sparse_layer(model)
-#
-#         # Косинусное сходство между градиентами ребер
-#         grad_values = last_layer.weight_values.grad
-#         similarities = torch.zeros(grad_values.size(0) - 1)
-#
-#         for i in range(similarities.size(0)):
-#             similarities[i] = F.cosine_similarity(
-#                 grad_values[i].unsqueeze(0),
-#                 grad_values[i + 1].unsqueeze(0),
-#                 dim=1
-#             )
-#
-#         model.zero_grad()
-#         return similarities
-
