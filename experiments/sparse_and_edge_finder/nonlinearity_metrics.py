75712309d39182343ea0d5bc5803c278e1828300
FeToR1 <fedornigretuk@gmail.com>
2024-11-19 19:49:05 +0300

    added: edge_finder

--- experiments/sparse_and_edge_finder/nonlinearity_metrics.py
+++ experiments/sparse_and_edge_finder/nonlinearity_metrics.py
@@ -1,34 +1,14 @@
 from abc import abstractmethod, ABC
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
 
 class NonlinearityMetric(ABC):
     def __init__(self, loss_fn):
         self.loss_fn = loss_fn
 
-    @staticmethod
-    def _get_last_sparse_layer(model):
-        # if not isinstance(model, SparseRecursiveLinear):
-        #     raise ValueError("Expected a SparseRecursiveLinear model.")
-        return model.sparse_linear
-
     @abstractmethod
     def calculate(self, model, X_arr, y_arr):
         pass
-
-    def __call__(self, model, X_arr, y_arr, top_k: int = 1):
-        model.eval()
-        last_layer = self._get_last_sparse_layer(model)
-        values = self.calculate(model, X_arr, y_arr)
-
-        # Индексы, отсортированные по убыванию значений метрики
-        sorted_indices = torch.argsort(values, descending=True)
-
-        # Возвращаем топ k индексов из last_layer.weight_indices
-        top_k_indices = last_layer.weight_indices[sorted_indices[:top_k]]
-        return top_k_indices
 
 
 # Метрика 1: Средний градиент для каждого ребра
@@ -41,7 +21,7 @@ class GradientMeanEdgeMetric(NonlinearityMetric):
         loss = self.loss_fn(y_pred, y_arr)
         loss.backward()
 
-        last_layer = self._get_last_sparse_layer(model)
+        last_layer = model.sparse_linear
 
         # Градиенты для разреженных весов
         edge_gradients = last_layer.weight_values.grad.abs()
@@ -61,7 +41,7 @@ class PerturbationSensitivityEdgeMetric(NonlinearityMetric):
         # Оригинальный вывод модели
         original_output = model(X_arr).detach()
 
-        last_layer = self._get_last_sparse_layer(model)
+        last_layer = model.sparse_linear
         sensitivities = torch.zeros_like(last_layer.weight_values)
 
         # Возмущение каждого веса
@@ -79,3 +59,5 @@ class PerturbationSensitivityEdgeMetric(NonlinearityMetric):
                 last_layer.weight_values[idx] = original_value
 
         return sensitivities
+
+
