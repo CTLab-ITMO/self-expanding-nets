c0bbec6b69ab8a96e56b028e2ac02284403f6758
FeToR1 <fedornigretuk@gmail.com>
2025-03-03 13:36:53 +0300

    fixed: gitignore

--- senmodel/model/utils.py
+++ senmodel/model/utils.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import torch
 from torch import nn
 
@@ -24,23 +25,24 @@ def dense_to_sparse(dense_tensor: torch.Tensor, device='cpu') -> torch.Tensor:
 
 
 def convert_dense_to_sparse_network(model: nn.Module, device='cpu', last_linear_module=None) -> nn.Module:
-    if last_linear_module is None:
-        last_linear_module = get_model_last_layer(model)
     
-    new_model = model.__class__()
 
-    for name, module in model.named_children():
+    new_model = deepcopy(model)  
+    
+    if last_linear_module is None:
+        last_linear_module = get_model_last_layer(new_model)
+
+    for name, module in new_model.named_children():
         if isinstance(module, nn.Linear):
             if module is last_linear_module:
                 sparse_weight = dense_to_sparse(module.weight.data, device=device)
                 sparse_bias = dense_to_sparse(module.bias.data, device=device)
                 setattr(new_model, name, ExpandingLinear(sparse_weight, sparse_bias, device=device))
-            else:
-                setattr(new_model, name, module)
         else:
             setattr(new_model, name, convert_dense_to_sparse_network(module, device=device, last_linear_module=last_linear_module))
-    
+
     return new_model
+
 
 
 def get_model_last_layer(model):
